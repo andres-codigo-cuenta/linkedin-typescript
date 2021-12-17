356ecbed92174b4397a474b9a07e240f720234d6
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-12-17 15:42:36 +1100

    [folder, html, ts, js] defining a class example

---
+++ 05-classes/02-defining-a-class/app.js
@@ -0,0 +1,27 @@
+/* From the last video...
+
+function TodoService() {
+    this.todos = [];
+}
+
+TodoService.prototype.getAll = function() {
+    return this.todos;
+}
+
+*/
+var TodoService = /** @class */ (function () {
+    function TodoService(todos) {
+        this.todos = todos;
+    }
+    TodoService.prototype.getAll = function () {
+        return this.todos;
+    };
+    return TodoService;
+}());
+var TodoState;
+(function (TodoState) {
+    TodoState[TodoState["New"] = 1] = "New";
+    TodoState[TodoState["Active"] = 2] = "Active";
+    TodoState[TodoState["Complete"] = 3] = "Complete";
+    TodoState[TodoState["Deleted"] = 4] = "Deleted";
+})(TodoState || (TodoState = {}));
