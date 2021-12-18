220d0d3a596a32e2783deb5da865824d8979125e
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-12-18 19:45:37 +1100

    [htnl, ts, js] control flow analysis and read only properties

---
+++ 02-upgrades-and-features/02-features-from-previous-versions/02-control-flow-analysis-and-read-only-properties/app.js
@@ -0,0 +1,10 @@
+var myControlFlow;
+myControlFlow = "Hello";
+console.log(typeof myControlFlow);
+myControlFlow = 4;
+console.log(typeof myControlFlow);
+var vreeg = { height: 100, width: 50, ocean: 'Pacifc' };
+console.log(vreeg);
+// Warning at the developer level
+vreeg.ocean = "Atlantic";
+console.log(vreeg);
