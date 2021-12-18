c460e139cf4d6ed5e14ed539cfb12796caeb3c55
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-12-18 11:44:13 +1100

    [folder, ts, js] using namespaces to encapsulate private members example

---
+++ 07-understanding-modules/03-using-namespaces-to-encapsulate-private-members/app.js
@@ -0,0 +1,11 @@
+var jQuery = {
+    version: 1.19,
+    fn: {}
+};
+(function defineType($) {
+    if ($.version < 1.15)
+        throw 'Plugin requires jQuery version 1.15+';
+    $.fn.myPlugin = function () {
+        // my plugin code
+    };
+})(jQuery);
